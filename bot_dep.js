e1fad66fc8864c1a9c045e5f7d2af2a674b62bcb
Дмитрий <dimabukov52@gmail.com>
2022-06-28 21:12:16 +0300

    update2_1_13

--- bot_dep.js
+++ bot_dep.js
@@ -9,27 +9,27 @@ let cron = require('node-cron')
 const fs = require("fs");
 
 
-cron.schedule('1 1 3 * * *', () => {
-    if (listId.table.length !== 0) {
-        for (let i = 0; i < listId.table.length; i++) {
-            bot.telegram.sendMessage(listId.table[i].id, 'Доброго ранку!')
-        }
-    }
-})
-cron.schedule('1 10 18 * * *', () =>{
-    if (listId.table.length !== 0) {
-        for (let i = 0; i < listId.table.length; i++) {
-            bot.telegram.sendMessage(listId.table[i].id, 'Вимкніть світло!')
-        }
-    }
-})
-cron.schedule('1 1 19 * * *', () =>{
-    if (listId.table.length !== 0) {
-        for (let i = 0; i < listId.table.length; i++) {
-            bot.telegram.sendMessage(listId.table[i].id, 'На добраніч!')
-        }
-    }
-})
+// cron.schedule('1 1 3 * * *', () => {
+//     if (listId.table.length !== 0) {
+//         for (let i = 0; i < listId.table.length; i++) {
+//             bot.telegram.sendMessage(listId.table[i].id, 'Доброго вечора!')
+//         }
+//     }
+// })
+// cron.schedule('1 1 18 * * *', () =>{
+//     if (listId.table.length !== 0) {
+//         for (let i = 0; i < listId.table.length; i++) {
+//             bot.telegram.sendMessage(listId.table[i].id, 'Вимкніть світло!')
+//         }
+//     }
+// })
+// cron.schedule('1 1 19 * * *', () =>{
+//     if (listId.table.length !== 0) {
+//         for (let i = 0; i < listId.table.length; i++) {
+//             bot.telegram.sendMessage(listId.table[i].id, 'На добраніч!')
+//         }
+//     }
+// })
 
 let workbook_new = new Excel.Workbook();
 let listJson = {table: []}
@@ -80,12 +80,12 @@ bot.command('doc', (ctx) => {
 bot.command('duty', async (ctx) => {
     obj.findMin()
     try {
-         ctx.reply('Йде їбашити курсант ' + listJson.table[index - 2].name + ', кількість нарядів : ' + listJson.table[index - 2].coundOfDuty, Markup.inlineKeyboard(
+        let messageI = await ctx.reply('Йде їбашити курсант ' + listJson.table[index - 2].name + ', кількість нарядів : ' + listJson.table[index - 2].coundOfDuty, Markup.inlineKeyboard(
             [Markup.button.callback('Помилувати', 'btn_1'), Markup.button.callback('Підтвердити', 'btn_2')],
         ));
         console.log(ctx.chat.id)
         chatId = ctx.chat.id
-        // messageId = messageI.message_id
+        messageId = messageI.message_id
 
     } catch (e) {
         console.error(e)
@@ -104,20 +104,20 @@ let obj = {
         }
 
     },
-     unable() {
-        // bot.telegram.deleteMessage(chatId, messageId)
+    async unable() {
+        bot.telegram.deleteMessage(chatId, messageId)
         if (index < listJson.table.length + 1) {
             index += 1;
         } else if (index === listJson.table.length + 1) {
             index = 2;
         }
-         bot.telegram.sendMessage(chatId, 'Йде їбашити курсант ' + listJson.table[index - 2].name + ', кількість нарядів : ' + listJson.table[index - 2].coundOfDuty, Markup.inlineKeyboard(
+        let message = await bot.telegram.sendMessage(chatId, 'Йде їбашити курсант ' + listJson.table[index - 2].name + ', кількість нарядів : ' + listJson.table[index - 2].coundOfDuty, Markup.inlineKeyboard(
             [Markup.button.callback('Помилувати', 'btn_1'), Markup.button.callback('Підтвердити', 'btn_2')]
         ))
-        // messageId = message.message_id
+        messageId = message.message_id
     },
-     able(i) {
-        // bot.telegram.deleteMessage(chatId, messageId)
+    async able(i) {
+        bot.telegram.deleteMessage(chatId, messageId)
         bot.telegram.sendMessage(chatId, 'Йде їбашити курсант ' + listJson.table[i].name + ', було нарядів : ' + listJson.table[i].coundOfDuty + ', а стане : ' + (listJson.table[i].coundOfDuty + 1))
         listJson.table[i].coundOfDuty += 1
         workbook_new.xlsx.readFile('./duty.xlsx')
