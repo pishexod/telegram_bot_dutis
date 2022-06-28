620fc040aaa212db4a3a61165f972734543000e3
Дмитрий <dimabukov52@gmail.com>
2022-06-28 21:00:01 +0300

    update2_1_10

--- bot_dep.js
+++ bot_dep.js
@@ -80,12 +80,12 @@ bot.command('doc', (ctx) => {
 bot.command('duty', async (ctx) => {
     obj.findMin()
     try {
-        let messageI = await ctx.reply('Йде їбашити курсант ' + listJson.table[index - 2].name + ', кількість нарядів : ' + listJson.table[index - 2].coundOfDuty, Markup.inlineKeyboard(
+         ctx.reply('Йде їбашити курсант ' + listJson.table[index - 2].name + ', кількість нарядів : ' + listJson.table[index - 2].coundOfDuty, Markup.inlineKeyboard(
             [Markup.button.callback('Помилувати', 'btn_1'), Markup.button.callback('Підтвердити', 'btn_2')],
         ));
         console.log(ctx.chat.id)
         chatId = ctx.chat.id
-        messageId = messageI.message_id
+        // messageId = messageI.message_id
 
     } catch (e) {
         console.error(e)
@@ -104,20 +104,20 @@ let obj = {
         }
 
     },
-    async unable() {
-        bot.telegram.deleteMessage(chatId, messageId)
+     unable() {
+        // bot.telegram.deleteMessage(chatId, messageId)
         if (index < listJson.table.length + 1) {
             index += 1;
         } else if (index === listJson.table.length + 1) {
             index = 2;
         }
-        let message = await bot.telegram.sendMessage(chatId, 'Йде їбашити курсант ' + listJson.table[index - 2].name + ', кількість нарядів : ' + listJson.table[index - 2].coundOfDuty, Markup.inlineKeyboard(
+         bot.telegram.sendMessage(chatId, 'Йде їбашити курсант ' + listJson.table[index - 2].name + ', кількість нарядів : ' + listJson.table[index - 2].coundOfDuty, Markup.inlineKeyboard(
             [Markup.button.callback('Помилувати', 'btn_1'), Markup.button.callback('Підтвердити', 'btn_2')]
         ))
-        messageId = message.message_id
+        // messageId = message.message_id
     },
-    async able(i) {
-        bot.telegram.deleteMessage(chatId, messageId)
+     able(i) {
+        // bot.telegram.deleteMessage(chatId, messageId)
         bot.telegram.sendMessage(chatId, 'Йде їбашити курсант ' + listJson.table[i].name + ', було нарядів : ' + listJson.table[i].coundOfDuty + ', а стане : ' + (listJson.table[i].coundOfDuty + 1))
         listJson.table[i].coundOfDuty += 1
         workbook_new.xlsx.readFile('./duty.xlsx')
